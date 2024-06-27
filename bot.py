6749e0ebe5db036b9c6ddcc039c683c65c395ae4
Croelofs <christopher.roelofs@gmail.com>
2024-06-27 11:07:23 -0400

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,78 @@
+# This example requires the 'message_content' intent.
+
+import discord
+from dotenv import load_dotenv
+import os
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+ignored_roles = ["The Crew", "Port Engineers","Administrator","Port Cadets","Developer"]
+
+common_keywords = ["port","ported","porting","want","coming","available","about","regarding","portmaster","game","uses","made","written","programmed"]
+
+listening_channels = ["bot-submersible","💡｜suggestion-talk"]
+
+generic_reponse = "This is an automated response. Please check the [new-suggestion](https://discord.com/channels/1122861252088172575/1232622554984878120) channel for more information on porting."
+
+response_mappings = [
+    {
+        "name": "PokeMMO",
+        "type": "text",
+        "keywords": ["pokemmo"],
+        "response": "PokeMMO can't be ported at this time."
+    },
+     {
+        "name": "Unity",
+        "type": "text",
+        "keywords": ["unity"],
+        "response": "Games using Unity can't be ported at this time."
+    },
+    {
+        "name": "San Andreas",
+        "type": "text",
+        "keywords": ["san","andreas","san andreas"],
+        "response": "Grand Theft Auto: San Andreas can't be ported at this time."
+    }
+]
+
+load_dotenv()
+
+def parseMessage(message):
+    message = message.lower()
+    is_common = False
+    for common in common_keywords:
+        if common in message:
+            is_common = True
+            break
+    if is_common:
+        for mapping in response_mappings:
+            for keyword in mapping["keywords"]:
+                if keyword in message:
+                    response = mapping["response"] + "\n\n" + generic_reponse
+                    return(response)
+                
+    return(False)
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    
+    if hasattr(message.author,"roles"):
+        for role in message.author.roles:
+            if role.name in ignored_roles:
+                return
+        
+    #if str(message.channel) in listening_channels:
+    response = parseMessage(message.content)
+    if response:
+        await message.reply(response)
+
+client.run(os.getenv('BOT_TOKEN'))
